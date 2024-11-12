cd0ac3c846db7babfc14fa578aee921c4f0ff780
Handerson Gleber <handerson.gleber@gmail.com>
2024-11-12 19:20:39 -0300

    versao sdl2

---
+++ build/CMakeFiles/DrawPixelSDL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DrawPixelSDL.dir/main.c.o"
+  "CMakeFiles/DrawPixelSDL.dir/main.c.o.d"
+  "DrawPixelSDL.exe"
+  "DrawPixelSDL.pdb"
+  "libDrawPixelSDL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DrawPixelSDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
